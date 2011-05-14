04ca9254fa03f442bfe42435a2888d62f3ba17af
Passi <chiruclan@t-online.de>
2011-05-14 20:11:49 +0200

    INIT

---
+++ ProgramDLer/main_frm.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main_frm
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main_frm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(3, -1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(508, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ProgramDLer v2 ... Initialization"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(3, 17)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(508, 12)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'main_frm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(513, 31)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "main_frm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ProgramDLer v2"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+
+End Class
---
+++ ProgramDLer/main_frm.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Net
+Public Class main_frm
+
+    Private WithEvents httpclient As WebClient
+
+    Private Sub main_frm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If My.Application.CommandLineArgs.Contains("-test") Then
+            MsgBox("ProgramDLer v2 funtkioniert! Klicke auf OK, um fortzufahren.")
+        End If
+        Try
+            Me.Text = "ProgramDLer v2 - " + My.Settings.download_file
+            httpclient = New WebClient
+
+            Dim SourceURL As String = My.Settings.download_url
+            Dim ZielDatei As String = My.Settings.download_file
+
+            httpclient.DownloadFileAsync(New Uri(SourceURL), ZielDatei)
+
+            ProgressBar1.Value = 0
+            ProgressBar1.Maximum = 100
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+
+    Private Sub httpclient_DownloadFileCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs) Handles httpclient.DownloadFileCompleted
+        Try
+            Dim p As New Process
+            p.StartInfo.FileName = My.Settings.download_file
+            p.Start()
+            Me.Close()
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+
+    Private Sub httpclient_DownloadProgressChanged(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles httpclient.DownloadProgressChanged
+        Try
+            ProgressBar1.Value = e.ProgressPercentage
+
+            Dim Totalbytes As Long = e.TotalBytesToReceive / 1024 / 1024
+            Dim Bytes As Long = e.BytesReceived / 1024 / 1024
+
+            Label1.Text = Bytes.ToString & " MB von " & Totalbytes.ToString & " MB (" & e.ProgressPercentage & "%)"
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+End Class
